5c3be56920bfba6a10a9e4bb01ddac1bc72901ea
vijai-veerapandian <vijai.veerapandian@gmail.com>
2025-04-14 16:55:12 -0400

    updated

--- tmp/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mytf-state-app-bucket"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
