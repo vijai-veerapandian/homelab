a68355e42efa5cdee85f500fe05918b548e29d27
vijai-veerapandian <vijai.veerapandian@gmail.com>
2025-04-16 14:26:30 -0400

    updated

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "demo_sg" {
   }
 
     ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow https inbound traffic"
